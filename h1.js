240ef205cb28b6c56e5a65a816a430749464668b
danwangde <746927725@qq.com>
2018-11-03 10:18:53 +0800

    css以及js拷贝相关知识

---
+++ h1.js
@@ -0,0 +1,10 @@
+
+window.myns=window.myns || {};
+
+(()=>{
+    function hello() {
+       console.log('hello word1')
+    };
+    window.myns.h1=hello;
+})()
+
---
+++ h2.js
@@ -0,0 +1,9 @@
+
+window.myns=window.myns || {};
+
+(()=>{
+    function hello() {
+        console.log('hello word2');
+    }
+    window.myns.h2=hello;
+})()
